d5f76f8905f10702eac286a415b378165999f020
Alvinfebrian2318 <alvinfebrian2318@gmail.com>
2021-04-14 13:21:23 +0700

    commit

---
+++ app/src/main/java/com/example/intent/PersonDetailActivity.java
@@ -0,0 +1,27 @@
+package com.example.intent;
+
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class PersonDetailActivity extends AppCompatActivity {
+
+    TextView nameAndAge, email, city;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_detail);
+
+        nameAndAge = findViewById(R.id.name_and_age);
+        email = findViewById(R.id.email);
+        city = findViewById(R.id.city);
+
+        Person user = getIntent().getParcelableExtra("user");
+        nameAndAge.setText(user.getName() + ", " + user.getAge());
+        email.setText(user.getEmail());
+        city.setText(user.getCity());
+    }
